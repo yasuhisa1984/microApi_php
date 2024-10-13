87e7c72b6a11d4f1031c757bc46f5cd09f8d5be4
yasuhisa1984 <penmouse275@gmail.com>
2024-10-13 23:11:26 +0900

    mysql8.2の導入

--- backend/src/routes.php
+++ backend/src/routes.php
@@ -16,4 +16,31 @@ return function (App $app) {
         $response->getBody()->write(json_encode($data));
         return $response->withHeader('Content-Type', 'application/json');
     });
+
+    // データベース接続とcustomerテーブルデータの取得
+    $app->get('/api/customers', function (Request $request, Response $response) {
+        $dsn = 'mysql:host=db;port=3306;dbname=sample';
+        $username = 'app';
+        $password = 'pass1234';
+
+        try {
+            $pdo = new PDO($dsn, $username, $password);
+            $statement = $pdo->query('SELECT * FROM customer');
+            $customers = [];
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                $customers[] = $row;
+            }
+            $pdo = null; // 切断
+
+            $response->getBody()->write(json_encode($customers));
+            return $response->withHeader('Content-Type', 'application/json');
+        } catch (PDOException $e) {
+            $error = [
+                'error' => true,
+                'message' => $e->getMessage(),
+            ];
+            $response->getBody()->write(json_encode($error));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+        }
+    });
 };
